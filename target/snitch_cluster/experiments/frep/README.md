8e3027d9cbcd7a43e3479531a8390336c3416a46
Luca Colagrande <luca.colagrande3@gmail.com>
2025-03-11 18:04:22 +0100

    Extend GEMM experiments with multiple HW configs

--- target/snitch_cluster/experiments/frep/README.md
+++ target/snitch_cluster/experiments/frep/README.md
@@ -20,3 +20,16 @@ make clean-vsim
 make PL_SIM=1 DEBUG=ON VCD_DUMP=1 bin/snitch_cluster.vsim
 ./experiments.py power.yaml --actions run power -j --run-dir pls
 ```
+
+To build all hardware configurations:
+```
+BIN_DIR=$PWD/experiments/frep/hw/zonl32fc/bin   make VSIM_BUILDDIR=experiments/frep/hw/zonl32fc/work-vsim   DEBUG=ON CFG_OVERRIDE=cfg/zonl32fc.hjson   $BIN_DIR/snitch_cluster.vsim
+BIN_DIR=$PWD/experiments/frep/hw/zonl64fc/bin   make VSIM_BUILDDIR=experiments/frep/hw/zonl64fc/work-vsim   DEBUG=ON CFG_OVERRIDE=cfg/zonl64fc.hjson   $BIN_DIR/snitch_cluster.vsim
+BIN_DIR=$PWD/experiments/frep/hw/zonl64dobu/bin make VSIM_BUILDDIR=experiments/frep/hw/zonl64dobu/work-vsim DEBUG=ON CFG_OVERRIDE=cfg/zonl64dobu.hjson $BIN_DIR/snitch_cluster.vsim
+BIN_DIR=$PWD/experiments/frep/hw/zonl48dobu/bin make VSIM_BUILDDIR=experiments/frep/hw/zonl48dobu/work-vsim DEBUG=ON CFG_OVERRIDE=cfg/zonl48dobu.hjson $BIN_DIR/snitch_cluster.vsim
+```
+
+Command to run a single experiment (from the experiment's run directory) for debugging:
+```
+../../../../../../../../../sw/blas/gemm/scripts/verify.py ../../../../../hw/zonl48dobu/bin/snitch_cluster.vsim ../../../../../build/zonl48dobu/24/32/8/gemm.elf
+```
--- target/snitch_cluster/experiments/frep/experiments.py
+++ target/snitch_cluster/experiments/frep/experiments.py
@@ -5,6 +5,7 @@
 #
 # Luca Colagrande <colluca@iis.ee.ethz.ch>
 
+from copy import deepcopy
 from snitch.target.SimResults import SimRegion
 from snitch.target.experiment_utils import ExperimentManager
 import random
@@ -12,6 +13,22 @@ import random
 from mako.template import Template
 from pathlib import Path
 
+NUM_GEMM_SIZES = 50
+HW_CFGS = [
+    'zonl32fc',
+    'zonl64fc',
+    'zonl64dobu',
+    # 'zonl48dobu',
+]
+NUM_TILES = 3
+ROI = 'tile_1'
+
+VSIM_BINS = {
+    'zonl32fc':   str(Path.cwd() / 'hw/zonl32fc/bin/snitch_cluster.vsim'),
+    'zonl64fc':   str(Path.cwd() / 'hw/zonl64fc/bin/snitch_cluster.vsim'),
+    'zonl64dobu': str(Path.cwd() / 'hw/zonl64dobu/bin/snitch_cluster.vsim'),
+    'zonl48dobu': str(Path.cwd() / 'hw/zonl48dobu/bin/snitch_cluster.vsim'),
+}
 DATA_DIR = Path('data').absolute()
 VERIFY_PY = Path('../../../../sw/blas/gemm/scripts/verify.py').absolute()
 
@@ -20,6 +37,7 @@ class FrepExperimentManager(ExperimentManager):
 
     def derive_axes(self, experiment):
         return {
+            'hw': experiment['hw'],
             'm': experiment['m'],
             'n': experiment['n'],
             'k': experiment['k'],
@@ -37,6 +55,9 @@ class FrepExperimentManager(ExperimentManager):
             f.write(cfg)
         return cfg_path
 
+    def derive_hw_cfg(self, experiment):
+        return 'cfg/' + experiment['hw'] + '.hjson'
+
 
 def generate_mat_size():
     sizes = list(range(8, 257, 8))
@@ -51,13 +72,14 @@ def calculate_total_size(m, n, k):
     return 2 * (a_size + b_size + c_size)
 
 
-def gen_experiment(experiments):
+def gen_experiments():
     # new layout requires checking that individual matrices fit in 8banks
     BANK_SIZE = 1 * 1024  # 2KiB
     MAX_ALLOWED_SIZE = 8 * BANK_SIZE  # Every matrix can take up maximum 8 banks
 
-    i = 0
-    while i < 10:
+    experiments = []
+    # TODO: filter repeated experiments
+    while len(experiments) < NUM_GEMM_SIZES:
         # generate random values in [8, 16, 24, ..., 256] for m, n, k
         m = generate_mat_size()
         n = generate_mat_size()
@@ -73,7 +95,6 @@ def gen_experiment(experiments):
 
         if max_size < MAX_ALLOWED_SIZE:
             experiments.append({'m': m, 'n': n, 'k': k})
-            i += 1
 
     return experiments
 
@@ -81,7 +102,7 @@ def gen_experiment(experiments):
 def get_average_fpu_util(row):
     util = []
     for i in range(0, 8):
-        util.append(row['results'].get_metric(SimRegion(f'hart_{i}', 'tile_1'), 'fpss_fpu_occupancy'))
+        util.append(row['results'].get_metric(SimRegion(f'hart_{i}', ROI), 'fpss_fpu_occupancy'))
     return sum(util) / len(util)
 
 
@@ -92,25 +113,32 @@ def main():
     random.seed(seed)
 
     # m, n and k are the dimensions of the tile
-    experiments = []
+    experiments = gen_experiments()
+    experiments = [deepcopy(exp) for _ in range(len(HW_CFGS)) for exp in experiments]
 
-    experiments = gen_experiment(experiments)
-
-    for experiment in experiments:
-        experiment['app'] = 'gemm'
-        experiment['m_tiles'] = 2
-        experiment['n_tiles'] = 1
-        experiment['cmd'] = [str(VERIFY_PY), "${sim_bin}", "${elf}"]
+    for i in range(len(HW_CFGS)):
+        for j in range(NUM_GEMM_SIZES):
+            k = i * NUM_GEMM_SIZES + j
+            experiments[k]['hw'] = HW_CFGS[i]
+            experiments[k]['app'] = 'gemm'
+            experiments[k]['m_tiles'] = NUM_TILES
+            experiments[k]['n_tiles'] = 1
+            experiments[k]['cmd'] = [str(VERIFY_PY), VSIM_BINS[HW_CFGS[i]], "${elf}"]
 
     manager = FrepExperimentManager(experiments)
     manager.run()
-    manager.export_power_experiments(SimRegion('hart_0', 'tile_1'))
+    # TODO: revisit the start and end times, probably makes sense to use the cluster barrier
+    # between tiles as a delimiter
+    manager.export_power_experiments(SimRegion('hart_0', ROI))
 
     df = manager.get_results()
     if manager.perf_results_available:
         df['fpu_util'] = df.apply(get_average_fpu_util, axis=1)
     df['size (KiB)'] = df.apply(lambda row: calculate_total_size(row['m'], row['n'], row['k']) / 1024, axis=1)
     print(df)
+
+    # Export results to file
+    df.to_csv('results.csv', index=False)
 
 
 if __name__ == '__main__':
